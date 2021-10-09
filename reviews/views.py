2b0451507755753896f81d0ec99e21e177b65850
NSale <gospasrb@yahoo.com>
2021-10-09 23:52:29 +0200

    add form class and default validation

--- reviews/views.py
+++ reviews/views.py
@@ -1,14 +1,20 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import ReviewForm
 
 
 # Create your views here.
 def review(request):
     if request.method == 'POST':
-        entered_username = request.POST['username']
-        print(entered_username)
-        return HttpResponseRedirect('thanks')
-    return render(request, 'reviews/review.html')
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect('thanks')
+    else:
+        form = ReviewForm()
+    return render(request, 'reviews/review.html', {
+        'form': form,
+    })
 
 
 def thank_you(request):
