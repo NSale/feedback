08095c5a23c4357868d638b211a0e8c70cf73a69
NSale <gospasrb@yahoo.com>
2021-10-12 00:22:35 +0200

    add review list and details page

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Feedback.as_view()),
+    path('', views.ReviewView.as_view()),
     path('thanks', views.ThankYou.as_view()),
+    path('reviews', views.ReviewList.as_view()),
+    path('reviews/<int:id>', views.ReviewDetails.as_view()),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -3,10 +3,11 @@ from django.views.generic.base import TemplateView
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import ReviewForm
+from .models import Feedback
 
 
 # Create your views here.
-class Feedback(View):
+class ReviewView(View):
 
     def get(self, request):
         form = ReviewForm()
@@ -31,4 +32,23 @@ class ThankYou(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['message'] = 'Thank you for submitting your feedback here'
+        return context
+
+
+class ReviewList(TemplateView):
+    template_name = 'reviews/review_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['reviews'] = Feedback.objects.all()
+        return context
+
+
+class ReviewDetails(TemplateView):
+    template_name = 'reviews/review_details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs['id']
+        context['review'] = Feedback.objects.get(pk=review_id)
         return context
